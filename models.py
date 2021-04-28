c20814637258ca580683df06f3bf0c39937cbac7
nilay1221 <nilay.sheth@somaiya.edu>
2021-04-29 01:22:07 +0530

    Updated Models

--- models.py
+++ models.py
@@ -1,13 +1,21 @@
+from sklearn.metrics import accuracy_score # evaluation metric
+from sklearn.metrics import f1_score # evaluation metric
 import joblib
 import pandas as pd
 from preprocessing import preprocessing
 import numpy as np
 
-loaded_model = joblib.load('models/rf.sav')
-transactions = pd.read_csv('data_part5.csv',index_col=0)
+rf_model = joblib.load('models/rf.sav')
+dt_model = joblib.load('models/decision_tree.sav')
+knn_model = joblib.load('models/knn.sav')
+lr_model = joblib.load('models/lr.sav')
+svm_model = joblib.load('models/svm.sav')
+# xgb_model = joblib.load('models/xgb.sav')
+
+transactions = pd.read_csv('datasets/data_part5.csv',index_col=0)
 processed_data = preprocessing(transactions)
 # print(processed_data[1:10])
-knn_output = loaded_model.predict(processed_data)
+knn_output = rf_model.predict(processed_data)
 # print(knn_output[knn_output==1])
 result = np.where(knn_output == 1)
 frauds_cases = result[0]
